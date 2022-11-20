5fb65f95ad25cf8b1c69f51398e062051b7266a8
icoo08217 <icoo08216@gmail.com>
2022-11-20 18:18:00 +0900

    [Feat]: DB sql문 추가

---
+++ DB/Init.sql
@@ -0,0 +1,9 @@
+# 개발 DB 생성
+DROP DATABASE IF EXISTS turtlebooks_dev;
+CREATE DATABASE turtlebooks_dev;
+USE turtlebooks_dev;
+
+# 테스트 DB 생성
+DROP DATABASE IF EXISTS turtlebooks_test;
+CREATE DATABASE turtlebooks_test;
+USE turtlebooks_test;
