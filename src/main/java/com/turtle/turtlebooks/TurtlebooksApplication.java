caabd777c497d5686089a7cb0e7a7d442edb9a91
icoo08217 <icoo08216@gmail.com>
2022-11-20 17:37:45 +0900

    [Chore]: Gradle 초기 설정 및 환경설정

---
+++ src/main/java/com/turtle/turtlebooks/TurtlebooksApplication.java
@@ -0,0 +1,13 @@
+package com.turtle.turtlebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtlebooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurtlebooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turtle/turtlebooks/TurtlebooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtle.turtlebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtlebooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
