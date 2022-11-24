dd3e4aef1d82bc4c4c13910f0a124dd60bee2977
icoo08217 <icoo08216@gmail.com>
2022-11-24 23:09:24 +0900

    [Feat]: PostKeyWord 엔티티 추가

---
+++ src/main/java/com/turtle/turtlebooks/app/postKeyWord/PostKeyWord.java
@@ -0,0 +1,24 @@
+package com.turtle.turtlebooks.app.postKeyWord;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class PostKeyWord {
+
+    @Id @GeneratedValue
+    private long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+
+    private String content;
+}
