37929782acbdda38082ab753eceb0bb6b0eea1bb
icoo08217 <icoo08216@gmail.com>
2022-11-30 20:53:48 +0900

    [Feat]:Rq 클래스 도입

---
+++ src/main/java/com/turtle/turtlebooks/app/base/rq/Rq.java
@@ -0,0 +1,32 @@
+package com.turtle.turtlebooks.app.base.rq;
+
+import com.turtle.turtlebooks.app.member.entity.Member;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@Slf4j
+@RequestScope
+public class Rq {
+
+    private final HttpServletRequest req;
+    private final HttpServletResponse resp;
+    private final MemberContext memberContext;
+
+    // 과제 : Rq 클래스 도입하기......
+    @Getter
+    private final Member member;
+
+    public Rq(HttpServletRequest req, HttpServletResponse resp) {
+        this.req = req;
+        this.resp = resp;
+
+        //현재 로그인한 회원의 인증정보를 가져옴.
+    }
+
+}
--- src/main/java/com/turtle/turtlebooks/app/member/controller/MemberController.java
+++ src/main/java/com/turtle/turtlebooks/app/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.turtle.turtlebooks.app.member.controller;
 
+import com.turtle.turtlebooks.app.base.rq.Rq;
+import com.turtle.turtlebooks.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+
+    private final MemberService memberService;
+    private final Rq rq;
 }
