65ce2c6b913b4e0dc2ff0dd8451ef47f665fbf54
icoo08217 <icoo08216@gmail.com>
2023-01-10 19:49:01 +0900

    [feat]: product 엔티티 개발

--- src/main/java/com/turtle/turtlebooks/app/product/entity/Product.java
+++ src/main/java/com/turtle/turtlebooks/app/product/entity/Product.java
@@ -1,21 +1,102 @@
 package com.turtle.turtlebooks.app.product.entity;
 
+import com.turtle.turtlebooks.app.AppConfig;
 import com.turtle.turtlebooks.app.base.entity.BaseEntity;
+import com.turtle.turtlebooks.app.member.entity.Member;
+import com.turtle.turtlebooks.app.postkeyword.entity.PostKeyword;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Entity
-public class Product extends BaseEntity {
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Product extends BaseEntity { // 도서 엔티티
 
-    // 회원 번호
-    private long authorId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member author;
 
-    // 글 키워드 번호
-    private long postKeywordId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PostKeyword postKeyword;
 
     private String subject;
 
     private int price;
 
+    public Product(long id) {
+        super(id);
+    }
+
+    public int getSalePrice() {
+        return getPrice();
+    }
+
+    public int getWholesalePrice() { // 도매가 계산
+        return (int) Math.ceil(getPrice() * AppConfig.getWholesalePriceRate());
+    }
+
+    public boolean isOrderable() {
+        return true;
+    }
+
+    public String getJdenticon() {
+        return "product__" + getId();
+    }
+
+    public String getExtra_inputValue_hashTagContents() {
+        Map<String, Object> extra = getExtra();
+
+        if (extra.containsKey("productTags") == false) {
+            return "";
+        }
+
+        List<ProductTag> productTags = (List<ProductTag>) extra.get("productTags");
+
+        if (productTags.isEmpty()) {
+            return "";
+        }
+
+        return productTags
+                .stream()
+                .map(productTag -> "#" + productTag.getProductKeyword().getContent())
+                .sorted()
+                .collect(Collectors.joining(" "));
+    }
+
+    public String getExtra_productTagLinks() {
+        Map<String, Object> extra = getExtra();
+
+        if (extra.containsKey("productTags") == false) {
+            return "";
+        }
+
+        List<ProductTag> productTags = (List<ProductTag>) extra.get("productTags");
+
+        if (productTags.isEmpty()) {
+            return "";
+        }
+
+        return productTags
+                .stream()
+                .map(productTag -> {
+                    String text = "#" + productTag.getProductKeyword().getContent();
+
+                    return """
+                            <a href="%s" class="text-link">%s</a>
+                            """
+                            .stripIndent()
+                            .formatted(productTag.getProductKeyword().getListUrl(), text);
+                })
+                .sorted()
+                .collect(Collectors.joining(" "));
+    }
 
 }
